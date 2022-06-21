e69e84adc62d639824bc8c298a7401fe32a21ae3
Harry Best <harry.best@softwire.com>
2022-06-21 10:45:55 +0100

    Add input for stopping number

--- app/src/main/java/com/example/fizzbuzz/main.kt
+++ app/src/main/java/com/example/fizzbuzz/main.kt
@@ -2,13 +2,30 @@ package com.example.fizzbuzz
 import kotlin.collections.listOf
 import kotlin.collections.reversed
 import kotlin.collections.joinToString
+import kotlin.system.exitProcess
 
 fun main() {
     // Ambiguities
     // should the requirement for Fezz to be in front on the first thing beginning with B apply if the output has been reversed by 17
     // Bong instruction says to only print bong but Fezz says to also print Fezz if bong is present which to listen to?
     // It's not clearly stated that Fizz should still apply if Fezz is printed
-    for (i in 1..1000) {
+    val DEFAULT_LIMIT = 1000;
+
+    print("Enter the maximum number: ")
+    var limit = DEFAULT_LIMIT;
+    try {
+        limit = Integer.valueOf(readLine())
+    } catch (e: Exception) {
+        print("Invalid integer")
+        exitProcess(1)
+    }
+
+    if (limit < 0) {
+        print("Limit must > 0")
+        exitProcess(1)
+    }
+
+    for (i in 1..limit) {
         var outputWords: List<String> = listOf<String>()
 
         if (i % 3 == 0) outputWords += "Fizz"
