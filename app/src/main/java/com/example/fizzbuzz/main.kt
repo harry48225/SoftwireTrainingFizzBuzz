503354ef8477c1f052a8d4dfeb8ed2d00e8ab6fe
Harry Best <harry.best@softwire.com>
2022-06-21 09:27:28 +0100

    Basic FizzBuzz

--- app/src/main/java/com/example/fizzbuzz/main.kt
+++ app/src/main/java/com/example/fizzbuzz/main.kt
@@ -1,5 +1,13 @@
 package com.example.fizzbuzz
 
 fun main() {
-    print("hello world!")
+    for (i in 1..100) {
+        var output = ""
+
+        if (i % 3 == 0) output += "Fizz"
+        if (i % 5 == 0) output += "Buzz"
+        if (output == "") output += i
+
+        println(output)
+    }
 }
